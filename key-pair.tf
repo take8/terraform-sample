f92ef971fff908be5d6ac390508037d7c0d35776
take8 <37664118+take8@users.noreply.github.com>
2024-08-11 18:05:39 +0900

    Update: キーペアファイルの設定の記述を改善

--- key-pair.tf
+++ key-pair.tf
@@ -16,18 +16,14 @@ resource "tls_private_key" "keygen" {
 }
 
 # local_fileのリソースを指定するとterraformを実行するディレクトリ内でファイル作成やコマンド実行が可能
-resource "local_file" "private_key_openssh" {
-  filename = local.private_key_file
-  content  = tls_private_key.keygen.private_key_openssh
-  provisioner "local-exec" {
-    command = "chmod 600 ${local.private_key_file}"
-  }
+resource "local_sensitive_file" "private_key_openssh" {
+  filename        = local.private_key_file
+  content         = tls_private_key.keygen.private_key_openssh
+  file_permission = "0600"
 }
 
 resource "local_file" "public_key_openssh" {
-  filename = local.public_key_file
-  content  = tls_private_key.keygen.public_key_openssh
-  provisioner "local-exec" {
-    command = "chmod 600 ${local.public_key_file}"
-  }
+  filename        = local.public_key_file
+  content         = tls_private_key.keygen.public_key_openssh
+  file_permission = "0600"
 }
